2e1e335e04cc58c1e94afccd9bc61f3acc3ef981
Hagit Ram <ram1hagit@gmail.com>
2020-03-11 15:03:00 +0200

    reversestring solved

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -2,10 +2,19 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let rvs = "";
+    for (let i = 1; i < str.length + 1 ; i++) {
+        rvs += str.substr(-1*i, 1);
+    }
+
+    return rvs;
+}
+
+// console.log(reverse("apple"));
 
 module.exports = reverse;
