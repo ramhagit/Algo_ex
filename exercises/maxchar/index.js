db83fffe47a93dfdc0a3f9deb72af9782a2a53d8
Hagit Ram <ram1hagit@gmail.com>
2020-03-23 17:56:48 +0200

    maxchar solved

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -7,10 +7,24 @@
 
 function maxChar(str) {
     const obj = {};
-    let max_char = '';
-    for (const char of str) {
 
+    for (const char of str) {
+        if (Object.keys(obj).includes(char)) {
+            obj[char] += 1;
+        } else {
+            obj[char] = 1;
+        }
     }
+
+    let max_char = Object.keys(obj)[0];
+
+    for (const key of Object.keys(obj)) {
+        if (obj[key] > obj[max_char]) {
+            max_char = key;
+        }
+    }
+
+    return max_char;
 }
 
 module.exports = maxChar;
