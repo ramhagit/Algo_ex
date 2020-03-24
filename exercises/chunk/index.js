0de0aec45f58e85c7ab04e05f5f54b95f16b22b9
Hagit Ram <ram1hagit@gmail.com>
2020-03-24 19:58:53 +0200

    chunk solved

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,20 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chunk_arr = [];
+    const sub_arr = array.length / size;
+    let index = 0;
+    for (let i = 0; i < sub_arr; i++) {
+        chunk_arr.push([]);
+        for (let j = 0; j < size; j++) {
+            if (index < array.length) {
+                chunk_arr[i].push(array[index]);
+                index++;
+            }
+        }
+    }
+    return chunk_arr;
+}
 
 module.exports = chunk;
