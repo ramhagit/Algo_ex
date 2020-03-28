99361886d65f9c5a1c867f27e36eaae909ea4d56
Hagit Ram <ram1hagit@gmail.com>
2020-03-28 15:40:39 +0300

    pyramid solved with recursion

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,24 +14,39 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-    const colSize = (n-1)*2 + 1;
-
-    for (let row = 0; row < n; row++) {
-        let stair = '';
-
-        for (let column = 0; column < colSize; column++) {
-
-            if (column >= n - 1 - row && column <= n - 1 + row) {
-                stair += '#';
-            } else {
-                stair += ' ';
-            }
-
-        }
-
-        console.log(stair);
+function pyramid(n, row = 0, stair = '') {
+    if (row === n) {
+        return;
     }
+
+    if (stair.length === (n-1)*2 + 1) {
+        console.log(stair);
+        return pyramid(n, row +1);
+    }
+
+    const add = (stair.length >= n-1 - row && stair.length <= n-1 + row) ? '#' : ' ' ;
+
+    stair += add;
+    pyramid(n, row, stair);
 }
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//     const colSize = (n-1)*2 + 1;
+//
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+//
+//         for (let column = 0; column < colSize; column++) {
+//
+//             if (column >= n - 1 - row && column <= n - 1 + row) {
+//                 stair += '#';
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+//
+//         console.log(stair);
+//     }
+// }
