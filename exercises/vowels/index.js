81deccabac9e581c9c1caad4ed3a1e7c29ee2fe4
Hagit Ram <ram1hagit@gmail.com>
2020-03-29 15:25:27 +0300

    vowels solved

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,14 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let counter = 0;
+
+    for (let char of str) {
+        counter += ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"].includes(char) ? 1 : 0 ;
+    }
+
+    return counter;
+}
 
 module.exports = vowels;
