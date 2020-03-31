b02c3c5b13814ce4678ee43a0b8993a75c7adb30
Hagit Ram <ram1hagit@gmail.com>
2020-04-01 01:47:32 +0300

    fib solved with minor bug

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,31 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+    let a = 0;
+    let b = 1;
+    let index = 1;
+    let flag = "";
+
+    if (n === 0) {
+        return 0;
+    }
+
+    // } else if (n === 1) {
+    //     return 1;
+    // }
+
+    while (index < n) {
+        a += b;
+        flag = "a";
+        index++;
+        b += a;
+        flag = "b";
+        index++;
+    }
+
+    console.log("n: ", n, "index: ", index, "a: ", a, "b: ", b, "flag: ", flag)
+    return flag === "a" ? a : b;
+}
 
 module.exports = fib;
